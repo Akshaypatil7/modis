3e654d6164d1312e9d181e2f0341932c73ea462d
Rodrigo Almeida <rodrigo.almeida@up42.com>
2019-09-25 15:26:21 +0200

    Format code

--- blocks/nasa_modis/src/modis.py
+++ blocks/nasa_modis/src/modis.py
@@ -10,12 +10,14 @@ from blockutils.geometry import filter_tiles_intersect_with_geometry
 from blockutils.stac import STACQuery
 from blockutils.logging import get_logger
 from blockutils.fetcher import AbstractFetcher, AbstractAOIClippedFetcher
-from blockutils.common import (load_query, save_metadata, ensure_data_directories_exist,
-                               BlockModes, get_block_mode)
+from blockutils.common import (
+    load_query, save_metadata, ensure_data_directories_exist, BlockModes, get_block_mode
+)
 
 from gibs import GibsAPI, extract_query_dates
 
 logger = get_logger(__name__)
+
 
 class Modis:
 
@@ -34,8 +36,6 @@ class Modis:
         def __init__(self):
             self.api = GibsAPI()
 
-        # pylint: disable=too-many-locals
-        # Variables for fetcher TODO
         def fetch(self, query: STACQuery, dry_run: bool = False) -> FeatureCollection:
 
             ensure_data_directories_exist()
@@ -48,10 +48,7 @@ class Modis:
             output_features: List[Feature] = []
 
             date_list = extract_query_dates(query)
-            layer_list = query.layers
 
-            logger.debug("Checking layer %r", layer_list)
-            are_valid, invalid_names, invalid_geom, valid_layers = self.api.validate_layers(layer_list, query.bounds())
             logger.debug("Checking layer %r", query.layers)
             are_valid, invalid, valid_layers = self.api.validate_layers(
                 query.layers, query.bounds()
@@ -68,17 +65,16 @@ class Modis:
             for query_date in date_list:
                 for layer in valid_layers:
                     feature_id: str = str(uuid.uuid4())
-                    return_poly = unary_union([box(*tuple(mercantile.bounds(bbox))) for bbox in tile_list])
-                    feature = Feature(id=feature_id,
-                                      bbox=return_poly.bounds,
-                                      geometry=return_poly)
+                    return_poly = unary_union(
+                        [box(*tuple(mercantile.bounds(bbox))) for bbox in tile_list]
+                    )
+                    feature = Feature(id=feature_id, bbox=return_poly.bounds, geometry=return_poly)
 
                     self.api.write_quicklook(layer, return_poly.bounds, query_date, feature_id)
 
                 if not dry_run:
                     # Fetch tiles and patch them together
-                    self.api.get_merged_image(valid_layers, tile_list, query_date,
-                                              feature_id)
+                    self.api.get_merged_image(valid_layers, tile_list, query_date, feature_id)
                     feature["properties"]["up42.data.aoiclipped"] = "%s.tif" % feature_id
 
                 logger.debug(feature)
