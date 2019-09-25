6b71d6adf803daf57661c9b82dbad63b68f6105c
Rodrigo Almeida <rodrigo.almeida@up42.com>
2019-09-25 10:04:12 +0200

    Format with yapf

--- blocks/nasa_modis/tests/test_gibs.py
+++ blocks/nasa_modis/tests/test_gibs.py
@@ -20,9 +20,11 @@ from context import GibsAPI, extract_query_dates, ensure_data_directories_exist,
 def fixture():
     ensure_data_directories_exist()
 
+
 @pytest.mark.live
 def test_get_capabilities_live():
     assert GibsAPI().get_capabilities().status_code == 200
+
 
 @pytest.mark.live
 def test_get_list_available_layers_live():
@@ -30,6 +32,7 @@ def test_get_list_available_layers_live():
     print(layers)
     print(len(layers))
     assert len(layers) >= 5
+
 
 def test_get_list_available_layers(requests_mock):
     _location_ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -45,6 +48,7 @@ def test_get_list_available_layers(requests_mock):
            == 'MODIS_Aqua_CorrectedReflectance_TrueColor'
     assert len(layers) == 45
 
+
 def test_validate_layers(requests_mock):
     _location_ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -53,8 +57,9 @@ def test_validate_layers(requests_mock):
 
     requests_mock.get(mock.ANY, content=fake_xml)
 
-    valid, i_names, i_geom, _ = GibsAPI().validate_layers(['MODIS_Aqua_CorrectedReflectance_TrueColor'],
-                                                          [50, 50, 60, 60])
+    valid, i_names, i_geom, _ = GibsAPI().validate_layers(
+        ['MODIS_Aqua_CorrectedReflectance_TrueColor'], [50, 50, 60, 60]
+    )
     assert valid
     assert i_names == i_geom == []
 
@@ -62,23 +67,31 @@ def test_validate_layers(requests_mock):
     assert not valid
     assert i_names == ['ABC']
 
-    multiple_valid, i_names, i_geom, _ = GibsAPI().validate_layers(['MODIS_Aqua_CorrectedReflectance_TrueColor',
-                                                                    'MODIS_Terra_CorrectedReflectance_TrueColor'],
-                                                                   [50, 50, 60, 60])
+    multiple_valid, i_names, i_geom, _ = GibsAPI().validate_layers(
+        ['MODIS_Aqua_CorrectedReflectance_TrueColor', 'MODIS_Terra_CorrectedReflectance_TrueColor'],
+        [50, 50, 60, 60]
+    )
     assert multiple_valid
     assert i_names == i_geom == []
 
-    multiple_valid, i_names, i_geom, _ = GibsAPI().validate_layers(['MODIS_Aqua_CorrectedReflectance_TrueColor',
-                                                                    'MODIS_Terra_CorrectedReflectance_TrueColor',
-                                                                    'ABC'], [50, 50, 60, 60])
+    multiple_valid, i_names, i_geom, _ = GibsAPI().validate_layers(
+        [
+            'MODIS_Aqua_CorrectedReflectance_TrueColor',
+            'MODIS_Terra_CorrectedReflectance_TrueColor', 'ABC'
+        ], [50, 50, 60, 60]
+    )
     assert not multiple_valid
     assert i_names == ['ABC']
 
-    multiple_geom, i_names, i_geom, _ = GibsAPI().validate_layers(['MODIS_Aqua_CorrectedReflectance_TrueColor'],
-                                                                  [200, 200, 210, 210])
+    multiple_geom, i_names, i_geom, _ = GibsAPI().validate_layers(
+        ['MODIS_Aqua_CorrectedReflectance_TrueColor'], [200, 200, 210, 210]
+    )
     assert not multiple_geom
     assert i_names == []
-    assert i_geom == ['POLYGON ((180 -85.051129, 180 85.051129, -180 85.051129, -180 -85.051129, 180 -85.051129))']
+    assert i_geom == [
+        'POLYGON ((180 -85.051129, 180 85.051129, -180 85.051129, -180 -85.051129, 180 -85.051129))'
+    ]
+
 
 def test_extract_query_dates():
     """
@@ -90,17 +103,16 @@ def test_extract_query_dates():
     """
 
     # case (1)
-    query = STACQuery.from_dict({
-
-        "zoom_level": 9,
-        "limit": 2,
-        "bbox": [
-            114.11227717995645,
-            -21.861101064554884,
-            114.20209027826787,
-            -21.764821237030162
-        ]
-    })
+    query = STACQuery.from_dict(
+        {
+            "zoom_level":
+                9,
+            "limit":
+                2,
+            "bbox":
+                [114.11227717995645, -21.861101064554884, 114.20209027826787, -21.764821237030162]
+        }
+    )
 
     date_list = extract_query_dates(query)
     assert len(date_list) == 2
@@ -109,49 +121,52 @@ def test_extract_query_dates():
         assert date.startswith('20')
 
     # case (2)
-    query = STACQuery.from_dict({
-        "zoom_level": 9,
-        "time": "2019-04-20T16:40:49+00:00/2019-04-25T17:45:49+00:00",
-        "limit": 2,
-        "bbox": [
-            114.11227717995645,
-            -21.861101064554884,
-            114.20209027826787,
-            -21.764821237030162
-        ]
-    })
+    query = STACQuery.from_dict(
+        {
+            "zoom_level":
+                9,
+            "time":
+                "2019-04-20T16:40:49+00:00/2019-04-25T17:45:49+00:00",
+            "limit":
+                2,
+            "bbox":
+                [114.11227717995645, -21.861101064554884, 114.20209027826787, -21.764821237030162]
+        }
+    )
 
     date_list = extract_query_dates(query)
     assert date_list == ['2019-04-24', '2019-04-25']
 
     # case (3)
-    query = STACQuery.from_dict({
-        "zoom_level": 9,
-        "time": "2019-04-01T16:40:49+00:00/2019-04-02T16:41:49+00:00",
-        "limit": 4,
-        "bbox": [
-            114.11227717995645,
-            -21.861101064554884,
-            114.20209027826787,
-            -21.764821237030162
-        ]
-    })
+    query = STACQuery.from_dict(
+        {
+            "zoom_level":
+                9,
+            "time":
+                "2019-04-01T16:40:49+00:00/2019-04-02T16:41:49+00:00",
+            "limit":
+                4,
+            "bbox":
+                [114.11227717995645, -21.861101064554884, 114.20209027826787, -21.764821237030162]
+        }
+    )
 
     date_list = extract_query_dates(query)
     assert date_list == ['2019-04-01', '2019-04-02']
 
     # case (4)
-    query = STACQuery.from_dict({
-        "zoom_level": 9,
-        "time": "2019-04-25T16:41:49+00:00",
-        "limit": 2,
-        "bbox": [
-            114.11227717995645,
-            -21.861101064554884,
-            114.20209027826787,
-            -21.764821237030162
-        ]
-    })
+    query = STACQuery.from_dict(
+        {
+            "zoom_level":
+                9,
+            "time":
+                "2019-04-25T16:41:49+00:00",
+            "limit":
+                2,
+            "bbox":
+                [114.11227717995645, -21.861101064554884, 114.20209027826787, -21.764821237030162]
+        }
+    )
 
     date_list = extract_query_dates(query)
     assert date_list == ['2019-04-25']
@@ -174,18 +189,30 @@ def test_download_wmts_tile_as_geotiff(requests_mock):
     result = GibsAPI().download_wmts_tile_as_geotiff(test_layer, test_date, test_tile)
 
     expected_meta = {
-        'driver': 'GTiff',
-        'dtype': 'uint8',
-        'nodata': None,
-        'width': 256,
-        'height': 256,
-        'count': 3,
-        'crs': CRS.from_dict(init='epsg:3857'),
-        'transform': Affine(305.74811314070394, 0.0, 2661231.5767766964,
-                            0.0, -305.7481131407094, -3443946.746416901)}
+        'driver':
+            'GTiff',
+        'dtype':
+            'uint8',
+        'nodata':
+            None,
+        'width':
+            256,
+        'height':
+            256,
+        'count':
+            3,
+        'crs':
+            CRS.from_dict(init='epsg:3857'),
+        'transform':
+            Affine(
+                305.74811314070394, 0.0, 2661231.5767766964, 0.0, -305.7481131407094,
+                -3443946.746416901
+            )
+    }
 
     with rio.open(result) as dataset:
         assert dataset.meta == expected_meta
+
 
 @pytest.mark.live
 def test_get_merged_image():
@@ -195,28 +222,45 @@ def test_get_merged_image():
     test_tiles = [mercantile.Tile(x=290, y=300, z=9), mercantile.Tile(x=290, y=301, z=9)]
 
     test_date = '2019-06-20'
-    test_layer = {"MODIS_Terra_CorrectedReflectance_TrueColor":{
-                  "Identifier": "MODIS_Terra_CorrectedReflectance_TrueColor",
-                  "TileMatrixSet": "GoogleMapsCompatible_Level9",
-                  "WGS84BoundingBox": "shapely.geometry.polygon.Polygon object at 0x130e71590",
-                  "Format": "jpeg"}}
+    test_layer = {
+        "MODIS_Terra_CorrectedReflectance_TrueColor":
+            {
+                "Identifier": "MODIS_Terra_CorrectedReflectance_TrueColor",
+                "TileMatrixSet": "GoogleMapsCompatible_Level9",
+                "WGS84BoundingBox": "shapely.geometry.polygon.Polygon object at 0x130e71590",
+                "Format": "jpeg"
+            }
+    }
 
-    result_filename = GibsAPI().get_merged_image(test_layer, test_tiles, test_date,
-                                                 "a8ebbe34-4d63-4eef-8ff4-c69da3ee359d")
+    result_filename = GibsAPI().get_merged_image(
+        test_layer, test_tiles, test_date, "a8ebbe34-4d63-4eef-8ff4-c69da3ee359d"
+    )
 
     expected_meta = {
-        'driver': 'GTiff',
-        'dtype': 'uint8',
-        'nodata': None,
-        'width': 256,
-        'height': 512,
-        'count': 3,
-        'crs': CRS.from_dict(init='epsg:3857'),
-        'transform': Affine(305.74811314070394, 0.0, 2661231.5767766964,
-                            0.0, -305.7481131407094, -3443946.746416901)}
+        'driver':
+            'GTiff',
+        'dtype':
+            'uint8',
+        'nodata':
+            None,
+        'width':
+            256,
+        'height':
+            512,
+        'count':
+            3,
+        'crs':
+            CRS.from_dict(init='epsg:3857'),
+        'transform':
+            Affine(
+                305.74811314070394, 0.0, 2661231.5767766964, 0.0, -305.7481131407094,
+                -3443946.746416901
+            )
+    }
 
     with rio.open(str(result_filename)) as dataset:
         assert dataset.meta == expected_meta
+
 
 @pytest.mark.live
 def test_get_multiple_merged_image():
@@ -227,21 +271,44 @@ def test_get_multiple_merged_image():
     test_tiles = [mercantile.Tile(x=290, y=300, z=9), mercantile.Tile(x=290, y=301, z=9)]
 
     test_date = '2019-06-20'
-    test_layers = {'MODIS_Terra_CorrectedReflectance_TrueColor':{'Identifier': 'MODIS_Terra_CorrectedReflectance_TrueColor',"Format": "jpeg"}, 'MODIS_Aqua_CorrectedReflectance_TrueColor':{'Identifier': 'MODIS_Aqua_CorrectedReflectance_TrueColor',"Format": "jpeg"}}
+    test_layers = {
+        'MODIS_Terra_CorrectedReflectance_TrueColor':
+            {
+                'Identifier': 'MODIS_Terra_CorrectedReflectance_TrueColor',
+                "Format": "jpeg"
+            },
+        'MODIS_Aqua_CorrectedReflectance_TrueColor':
+            {
+                'Identifier': 'MODIS_Aqua_CorrectedReflectance_TrueColor',
+                "Format": "jpeg"
+            }
+    }
 
-    result_filename = GibsAPI().get_merged_image(test_layers, test_tiles, test_date,
-                                                 "a8ebbe34-4d63-4eef-8ff4-c69da3ee359d")
+    result_filename = GibsAPI().get_merged_image(
+        test_layers, test_tiles, test_date, "a8ebbe34-4d63-4eef-8ff4-c69da3ee359d"
+    )
 
     expected_meta = {
-        'driver': 'GTiff',
-        'dtype': 'uint8',
-        'nodata': None,
-        'width': 256,
-        'height': 512,
-        'count': 6,
-        'crs': CRS.from_dict(init='epsg:3857'),
-        'transform': Affine(305.74811314070394, 0.0, 2661231.5767766964,
-                            0.0, -305.7481131407094, -3443946.746416901)}
+        'driver':
+            'GTiff',
+        'dtype':
+            'uint8',
+        'nodata':
+            None,
+        'width':
+            256,
+        'height':
+            512,
+        'count':
+            6,
+        'crs':
+            CRS.from_dict(init='epsg:3857'),
+        'transform':
+            Affine(
+                305.74811314070394, 0.0, 2661231.5767766964, 0.0, -305.7481131407094,
+                -3443946.746416901
+            )
+    }
 
     with rio.open(str(result_filename)) as dataset:
         assert dataset.meta == expected_meta
@@ -257,7 +324,7 @@ def test_write_quicklook():
 
     GibsAPI().write_quicklook(test_layer, test_bbox, test_query_date, test_uuid)
 
-    quicklook_path = "/tmp/quicklooks/%s.jpg" %test_uuid
+    quicklook_path = "/tmp/quicklooks/%s.jpg" % test_uuid
 
     assert os.path.isfile(quicklook_path)
 
