5f57114115a0756f1098759e8ad853eef2629a94
Markus Müller <markus.mueller@up42.com>
2020-01-09 08:09:22 +0100

    Adds blocks-style test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Hrm, if we don't delete the cache the linter skips all if its test cases as it has problems with our conftest
+rm -r .pytest_cache
+black .
+python -m pytest --pylint --pylint-rcfile=../../pylintrc --mypy --mypy-ignore-missing-imports --cov=src/
+coverage-badge -f -o coverage.svg
