4c8f5c72ecb60ef58ef5bde60f25112f5a172448
marooned20 <prayag.thakkar@up42.com>
2020-05-14 11:11:52 +0200

    rearrange path to prevent import failures

--- tests/context.py
+++ tests/context.py
@@ -10,8 +10,9 @@ import sys
 from blockutils.common import ensure_data_directories_exist
 from blockutils.stac import STACQuery
 
-from src.gibs import GibsAPI, extract_query_dates, make_list_layer_band
-from src.modis import Modis
-
+# NOTE: this must be before the modis and gibs imports - else tests will not find path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+from src.gibs import GibsAPI, extract_query_dates, make_list_layer_band
+from src.modis import Modis
